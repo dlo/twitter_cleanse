99eea4a31f022bff2c635708f2e119ae77b51488
Dan Loewenherz <dloewenherz@gmail.com>
2015-11-29 18:14:28 -0600

    bump up version to 0.0.5

--- twitter_cleanse/metadata.py
+++ twitter_cleanse/metadata.py
@@ -1,6 +1,6 @@
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __author__ = "Dan Loewenherz"
-__copyright__ = "Copyright 2015, Lionheart Software"
+__copyright__ = "Copyright 2015, Lionheart Software LLC"
 __maintainer__ = "Dan Loewenherz"
 __email__ = "dan@lionheartsw.com"
 __license__ = "Apache 2.0"
